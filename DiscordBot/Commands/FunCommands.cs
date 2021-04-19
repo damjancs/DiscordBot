1410eb8d8eddd1c30b86e624907bfee7ef325674
Damian Stańczyk <ddanekk96@gmail.com>
2021-04-19 14:32:50 +0200

    Add project files.

---
+++ DiscordBot/Commands/FunCommands.cs
@@ -0,0 +1,99 @@
+﻿using DiscordBot.DiscordBotLogic;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+using DSharpPlus.Interactivity.Extensions;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DiscordBot.Commands
+{
+    public class FunCommands : BaseCommandModule
+    {
+        [Command("ping")]
+        [Description("Returns pong.")]
+        public async Task Ping(CommandContext ctx) 
+        {
+            await ctx.Channel.SendMessageAsync("Pong").ConfigureAwait(false);
+        }
+        
+        [Command("add")]
+        [Description("Adds to numbers together.")]
+        public async Task Add(CommandContext ctx,
+            [Description("First number.")] int numberOne,
+            [Description("Second number.")] int numberTwo)
+        {
+            await ctx.Channel
+                .SendMessageAsync((numberOne + numberTwo).ToString())
+                .ConfigureAwait(false);
+        }
+
+        [Command("respondmessage")]
+        public async Task RespondMessage(CommandContext ctx)
+        {
+            var interactivity = ctx.Client.GetInteractivity();
+
+            var message = await interactivity.WaitForMessageAsync(x => x.Channel == ctx.Channel).ConfigureAwait(false);
+
+            await ctx.Channel.SendMessageAsync(message.Result.Content);
+        }
+
+        [Command("respondreaction")]
+        public async Task RespondReaction(CommandContext ctx)
+        {
+            var interactivity = ctx.Client.GetInteractivity();
+
+            var message = await interactivity.WaitForReactionAsync(x => x.Channel == ctx.Channel).ConfigureAwait(false);
+
+            await ctx.Channel.SendMessageAsync(message.Result.Emoji);
+        }
+
+        [Command("poll")]
+        public async Task Poll(CommandContext ctx, TimeSpan duration, params DiscordEmoji[] emojiOptions)
+        {
+            var interactivity = ctx.Client.GetInteractivity();
+            var options = emojiOptions.Select(x => x.ToString());
+
+            var pollEmbed = new DiscordEmbedBuilder
+            {
+                Title = "Poll",
+                Description = string.Join(" ", options)
+            };
+
+            var pollMessage = await ctx.Channel.SendMessageAsync(embed: pollEmbed).ConfigureAwait(false);
+
+            foreach (var option in emojiOptions)
+            {
+                await pollMessage.CreateReactionAsync(option).ConfigureAwait(false);
+            }
+
+            var result = await interactivity.CollectReactionsAsync(pollMessage, duration).ConfigureAwait(false);
+            var distinctResult = result.Distinct();
+
+            var results = distinctResult.Select(x => $"{x.Emoji}: {x.Total}");
+
+            await ctx.Channel.SendMessageAsync(string.Join("\n", results)).ConfigureAwait(false);
+
+        }
+
+        [Command("elo")]
+        public async Task Elo(CommandContext ctx, string playerName)
+        {            
+            await ctx.Channel.SendMessageAsync($"{playerName} : {BotLogic.ObtainFaceitElo(playerName)} ELO points.").ConfigureAwait(false);
+        }
+
+        [Command("last")]
+        public async Task LastMatch(CommandContext ctx, string playerName)
+        {
+
+            await ctx.Channel.SendMessageAsync($"Last match stats:\n" +
+                $"RESULT: \t{BotLogic.ObtainLastMatchResult(playerName)}\n" +
+                $"MAP: \t{BotLogic.ObtainLastMatchMap(playerName)}\n" +
+                $"SCORE: \t{BotLogic.ObtainLastMatchScore(playerName)}")
+                .ConfigureAwait(false);
+        }
+
+
+    }
+}
---
+++ DiscordBot/DiscordBotLogic/BotLogic.cs
@@ -0,0 +1,58 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DiscordBot.DiscordBotLogic
+{
+    public class BotLogic
+    {
+
+        public static string ObtainFaceitElo(string playerName)
+        {
+            string url = $"https://faceitstats.com/player/{playerName}";
+            var web = new HtmlAgilityPack.HtmlWeb();
+            HtmlDocument doc = web.Load(url);
+
+            string elo = doc.DocumentNode.SelectNodes("//*[@id=\"app\"]/main/div/div[1]/div[2]/div[1]/div/div[1]/h5")[0].InnerText;
+
+            return elo;
+        }
+
+        public static string ObtainLastMatchResult(string playerName)
+        {
+            string url = $"https://faceitstats.com/player/{playerName}";
+            var web = new HtmlAgilityPack.HtmlWeb();
+            HtmlDocument doc = web.Load(url);
+
+            string result = doc.DocumentNode.SelectNodes("//*[@id=\"app\"]/main/div/div[7]/div/table/tbody/tr[1]/td[1]/b" )[0].InnerText;           
+            string map = doc.DocumentNode.SelectNodes("//*[@id=\"app\"]/main/div/div[7]/div/table/tbody/tr[1]/td[8]")[0].InnerText;
+            string results = doc.DocumentNode.SelectNodes("//*[@id=\"app\"]/main/div/div[7]/div/table/tbody/tr[1]")[0].InnerText;
+
+            return result;
+        }
+
+        public static string ObtainLastMatchMap(string playerName)
+        {
+            string url = $"https://faceitstats.com/player/{playerName}";
+            var web = new HtmlAgilityPack.HtmlWeb();
+            HtmlDocument doc = web.Load(url);
+
+            string map = doc.DocumentNode.SelectNodes("//*[@id=\"app\"]/main/div/div[7]/div/table/tbody/tr[1]/td[8]")[0].InnerText;
+
+            return map;
+        }
+
+        public static string ObtainLastMatchScore(string playerName)
+        {
+            string url = $"https://faceitstats.com/player/{playerName}";
+            var web = new HtmlAgilityPack.HtmlWeb();
+            HtmlDocument doc = web.Load(url);
+
+            string score = doc.DocumentNode.SelectNodes("//*[@id=\"app\"]/main/div/div[7]/div/table/tbody/tr[1]/td[3]")[0].InnerText;
+
+            return score;
+        }
+    }
+
+}
